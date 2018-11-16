789a885ce1c8619562e481d80702cf1af52fa4d6
hyk0116i <hyunki8389@gmail.com>
2018-11-16 21:15:30 +0900

    메인

---
+++ src/RythemGame/Main.java
@@ -0,0 +1,15 @@
+package RythemGame;
+
+public class Main {
+
+	public static final int SCREEN_WIDTH=1280;
+	public static final int SCREEn_HEIGHT=720;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		
+		new RythemGame();
+	}
+
+
+}
